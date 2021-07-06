e6b44cfff1c992337b51af2e00bdaa35b1ef4d96
Charles <ky6n74o@hotmail.com>
2021-07-07 03:12:51 +0800

    first commit

---
+++ testpush.py
@@ -0,0 +1,9 @@
+Python 3.9.4 (v3.9.4:1f2e3088f3, Apr  4 2021, 12:32:44) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=1
+>>> b=2
+>>> a=b
+>>> b
+2
+>>> 
